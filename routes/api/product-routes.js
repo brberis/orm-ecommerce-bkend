0170edc02f9de8424988ad593a4c4bc3f0cd97b8
Cristobal Barberis <cristobal@barberis.com>
2022-09-03 23:49:29 -0400

    bulkcreate tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
+const { Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   Product.findAll()
@@ -10,16 +10,83 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  // expects {product_name: 'Some Product', price: 123, stock: 1, category_is: 1}
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.category_id,
-  
+router.get('/:id', (req, res) => {
+  Product.findAll()
+  .findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      }
+    ]
   })
-    .then(dbProductData => res.json(dbProductData))
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // expects {product_name: 'Some Product', price: 123, stock: 1, category_is: 1, tagIds: [1,2,3,4]}
+  Product.create(req.body)
+    .then(product => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+
+router.put('/:id', (req, res) => {
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
